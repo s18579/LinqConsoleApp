16c8693bf702955a5438df45dcce0cfa2c67f6f0
Mateusz Bednarek <s18579@pjwstk.edu.pl>
2020-05-08 19:57:08 +0200

    Dodaj pliki projektów.

---
+++ LinqConsoleApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace LinqConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var d = new LinqSamples();
+            Console.WriteLine("Zadanie 1:");
+            d.Przyklad1();
+            Console.WriteLine("\nZadanie 2:");
+            d.Przyklad2();
+            Console.WriteLine("\nZadanie 3:");
+            d.Przyklad3();
+            Console.WriteLine("\nZadanie 4:");
+            d.Przyklad4();
+            Console.WriteLine("\nZadanie 5:");
+            d.Przyklad5();
+            Console.WriteLine("\nZadanie 6:");
+            d.Przyklad6();
+            Console.WriteLine("\nZadanie 7:");
+            d.Przyklad7();
+            Console.WriteLine("\nZadanie 8:");
+            d.Przyklad8();
+            Console.WriteLine("\nZadanie 9:");
+            d.Przyklad9();
+            Console.WriteLine("\nZadanie 10:");
+            d.Przyklad10Button_Click();
+            Console.WriteLine("\nZadanie 11:");
+            d.Przyklad11();
+            Console.WriteLine("\nZadanie 12:");
+            d.Przyklad12();
+        }
+    }
+}
